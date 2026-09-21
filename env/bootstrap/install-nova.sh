4a4bc326017d9ffbbf281723e3a0d2d877cc6087
GK72 <gk.project72@gmail.com>
2026-09-21 19:13:41 +0200

    Misc: Bootstrapping Baldr in Docker image

---
+++ env/bootstrap/install-nova.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+conan export deps/nova-cpp/libnova/
---
+++ tools/create-build-image.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+PROJECT_DIR=$(git -C "${SCRIPT_DIR}" rev-parse --show-toplevel)
+
+docker build "${PROJECT_DIR}/env" -t cpp-toolchain:latest
