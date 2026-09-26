b8f01736c4df1b67bb68357125e4079ceda849d4
Ákos Virágh <viragh.akos21@gmail.com>
2026-09-26 14:29:25 +0200

    ci: Create CI XVI

--- settings.cmake
+++ settings.cmake
@@ -3,5 +3,5 @@ set(CMAKE_CXX_STANDARD 23)
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -g")
 
 if(COVERAGE)
-    set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-g -O0 -fno-inline -fprofile-arcs -ftest-coverage")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O0 -fno-inline -fprofile-arcs -ftest-coverage")
 endif()
